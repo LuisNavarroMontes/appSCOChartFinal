bf9ebb231f4585e4c9ba5b13fa05304770b3d289
LuisNavarroMontes <luis.navarro.montes@gmail.com>
2024-02-18 20:26:49 +0100

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js/auto';
+import { Socket } from 'ngx-socket-io';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'appSCOChartFinal';
+  chart: any = [];
+  constructor(
+    private socket: Socket
+  ) {
+  }
+  onSelect(data: any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  ngOnInit() {
+    this.createChart();
+      this.socket.on('message', (data:any) => {
+        var jsonObject = JSON.parse(data);
+        console.log('Mensaje del servidor:', jsonObject);
+        this.chart.data.datasets[0].data.push(jsonObject.temperatura);
+        this.chart.update();
+      });
+  }
+
+  onActivate(data: any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data: any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
+  createChart(){
+    
+    this.chart = new Chart("MyChart", {
+      type: 'line', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: Array.from({length: 100}, (_, index) => index + 1), 
+	       datasets: [
+          {
+            label: "Temperatura",
+            data: [10, 20, NaN, 40, 50, 60, 70, 80, 90, 100],
+            backgroundColor: 'blue'
+          },  
+        ]
+      },
+      options: {
+        aspectRatio:2.5
+      }
+      
+    });
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
+const config: SocketIoConfig = { url: 'https://receptor-92fkz82us-luis-projects-b8acb995.vercel.app/:4000', options: {
+    withCredentials: false,
+} };
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
